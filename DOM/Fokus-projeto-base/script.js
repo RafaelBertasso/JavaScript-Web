90e1ff7d1a0cd6d9f5549255ec084a70cd2fd01b
Bowie <Rafaelbertasso06@gmail.com>
2024-01-20 15:46:34 -0300

    aula dom js

--- DOM/Fokus-projeto-base/script.js
+++ DOM/Fokus-projeto-base/script.js
@@ -3,6 +3,7 @@ const focoBotao = document.querySelector('.app__card-button--foco')
 const curtoBotao = document.querySelector('.app__card-button--curto')
 const longoBotao = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 // adiciona um evento que será acionado sempre que um evento especifico aconteca (nesse caso, o clique)
 // elemento.addEventListener(evento, callback)
@@ -28,4 +29,20 @@ longoBotao.addEventListener('click', () => {
 function alterarContexto (contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            //innerHTML --> inserir textos, classes e tags e fazer listas
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada? <br>
+            <strong class="app__title-strong">Faça uma pausa curta </strong>`
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície. <br> 
+            <strong class="app__title-strong">Faça uma pausa longa </strong>`
+        default:
+            break;
+    }
 }
